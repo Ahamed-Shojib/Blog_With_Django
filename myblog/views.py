cad3fdd477b01520dc6cd467a222633ecd8ea3c6
Mehedi Hasan Shojib <ahamedshojib333@gmail.com>
2025-02-26 21:40:29 +0600

    Add SignUp Form with Bootstrap

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse 
 from django.urls import reverse 
-from django.shortcuts import HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect,render
 
 
 def home(request):
-    return HttpResponseRedirect(reverse('App_Blog:blog_list'))
+    #return HttpResponseRedirect(reverse('App_Blog:blog_list'))
     #return HttpResponse('Welcome to Home Page')
+    return render(request,'base.html')
